844b765df83d8ee473e4e63933f67a5e4acfe4a4
haakym <haakym@gmail.com>
2016-04-26 20:24:17 +0100

    dates as props initialise on construct, array, json methods with tests

--- src/UKBankHoliday.php
+++ src/UKBankHoliday.php
@@ -1,202 +1,234 @@
-<?php namespace Haakym\UKBankHoliday;
+<?php
+namespace Haakym\UKBankHoliday;
 
-class UKBankHoliday {
+use JsonSerializable;
 
-    protected $yr;
+class UKBankHoliday implements JsonSerializable {
 
-    public function __construct($yr)
+    protected $year;
+    protected $newYears;
+    protected $goodFriday;
+    protected $easterMonday;
+    protected $mayDay;
+    protected $whitSun;
+    protected $summerBankHoliday;
+    protected $christmas;
+    protected $boxingDay;
+    protected $milleniumEve;
+
+    public function __construct($year = null)
     {
-        $this->yr = $yr;
+        $this->year = $year ?: date("Y");
+        $this->newYears = $this->newYears($this->year);
+        $this->goodFriday = $this->goodFriday($this->year);
+        $this->easterMonday = $this->easterMonday($this->year);
+        $this->mayDay = $this->mayDay($this->year);
+        $this->whitSun = $this->whitSun($this->year);        
+        $this->summerBankHoliday = $this->summerBankHoliday($this->year);
+        $this->christmas = $this->christmas($this->year);
+        $this->boxingDay = $this->boxingDay($this->year);
+        $this->milleniumEve = ($this->year == 1999) ? "1999-12-31" : null;
     }
 
-    public function newYears($year)
+    public function year($year)
     {
-        $newYearsDay = date("w", strtotime("{$year}-01-01 12:00:00"));
+        $this->year = $year;
+    }
+
+    public function newYears()
+    {
+        $newYearsDay = date("w", strtotime("{$this->year}-01-01 12:00:00"));
 
         switch ($newYearsDay) {
             case 6:
-                $date = "{$year}-01-03";
+                $date = "{$this->year}-01-03";
                 break;
             case 0:
-                $date = "{$year}-01-02";
+                $date = "{$this->year}-01-02";
                 break;
             default:
-                $date = "{$year}-01-01";
+                $date = "{$this->year}-01-01";
         }
 
         return $date;
     }
 
-    public function goodFriday($year)
+    public function goodFriday()
     {
-        return date("Y-m-d", strtotime( "+".(easter_days($year) - 2)." days", strtotime("{$year}-03-21 12:00:00") ));
+        return date("Y-m-d", strtotime( "+".(easter_days($this->year) - 2)." days", strtotime("{$this->year}-03-21 12:00:00") ));
     }
 
-    public function easterMonday($year)
+    public function easterMonday()
     {
-        return date("Y-m-d", strtotime( "+".(easter_days($this->yr) + 1)." days", strtotime("{$year}-03-21 12:00:00") ));
+        return date("Y-m-d", strtotime( "+".(easter_days($this->year) + 1)." days", strtotime("{$this->year}-03-21 12:00:00") ));
     }
 
-    public function mayDay($year)
+    public function mayDay()
     {
         $mayDay = '';
 
-        if ($year == 1995) {
+        if ($this->year == 1995) {
             $mayDay = "1995-05-08"; // VE day 50th anniversary year exception
         } else {
-            switch (date("w", strtotime("{$year}-05-01 12:00:00"))) {
+            switch (date("w", strtotime("{$this->year}-05-01 12:00:00"))) {
                 case 0:
-                    $mayDay = "{$year}-05-02";
+                    $mayDay = "{$this->year}-05-02";
                     break;
                 case 1:
-                    $mayDay = "{$year}-05-01";
+                    $mayDay = "{$this->year}-05-01";
                     break;
                 case 2:
-                    $mayDay = "{$year}-05-07";
+                    $mayDay = "{$this->year}-05-07";
                     break;
                 case 3:
-                    $mayDay = "{$year}-05-06";
+                    $mayDay = "{$this->year}-05-06";
                     break;
                 case 4:
-                    $mayDay = "{$year}-05-05";
+                    $mayDay = "{$this->year}-05-05";
                     break;
                 case 5:
-                    $mayDay = "{$year}-05-04";
+                    $mayDay = "{$this->year}-05-04";
                     break;
                 case 6:
-                    $mayDay = "{$year}-05-03";
+                    $mayDay = "{$this->year}-05-03";
                     break;
             }
         }
         return $mayDay;
     }
 
-    public function whitSun($year)
+    public function whitSun()
     {
         $whitSun = '';
 
-        if ($year == 2002) { // exception year
+        if ($this->year == 2002) { // exception year
             $whitSun = "2002-06-03";
             $whitSun = "2002-06-04";
         } else {
-            switch (date("w", strtotime("{$year}-05-31 12:00:00"))) {
+            switch (date("w", strtotime("{$this->year}-05-31 12:00:00"))) {
                 case 0:
-                    $whitSun = "{$year}-05-25";
+                    $whitSun = "{$this->year}-05-25";
                     break;
                 case 1:
-                    $whitSun = "{$year}-05-31";
+                    $whitSun = "{$this->year}-05-31";
                     break;
                 case 2:
-                    $whitSun = "{$year}-05-30";
+                    $whitSun = "{$this->year}-05-30";
                     break;
                 case 3:
-                    $whitSun = "{$year}-05-29";
+                    $whitSun = "{$this->year}-05-29";
                     break;
                 case 4:
-                    $whitSun = "{$year}-05-28";
+                    $whitSun = "{$this->year}-05-28";
                     break;
                 case 5:
-                    $whitSun = "{$year}-05-27";
+                    $whitSun = "{$this->year}-05-27";
                     break;
                 case 6:
-                    $whitSun = "{$year}-05-26";
+                    $whitSun = "{$this->year}-05-26";
                     break;
             }
         }
         return $whitSun;
     }
 
-    public function summerBankHoliday($year)
+    public function summerBankHoliday()
     {
         $summerBankHoliday = '';
 
-        switch (date("w", strtotime("{$year}-08-31 12:00:00"))) {
+        switch (date("w", strtotime("{$this->year}-08-31 12:00:00"))) {
             case 0:
-                $summerBankHoliday = "{$year}-08-25";
+                $summerBankHoliday = "{$this->year}-08-25";
                 break;
             case 1:
-                $summerBankHoliday = "{$year}-08-31";
+                $summerBankHoliday = "{$this->year}-08-31";
                 break;
             case 2:
-                $summerBankHoliday = "{$year}-08-30";
+                $summerBankHoliday = "{$this->year}-08-30";
                 break;
             case 3:
-                $summerBankHoliday = "{$year}-08-29";
+                $summerBankHoliday = "{$this->year}-08-29";
                 break;
             case 4:
-                $summerBankHoliday = "{$year}-08-28";
+                $summerBankHoliday = "{$this->year}-08-28";
                 break;
             case 5:
-                $summerBankHoliday = "{$year}-08-27";
+                $summerBankHoliday = "{$this->year}-08-27";
                 break;
             case 6:
-                $summerBankHoliday = "{$year}-08-26";
+                $summerBankHoliday = "{$this->year}-08-26";
                 break;
         }
         return $summerBankHoliday;
     }
 
-    public function christmas($year)
+    public function christmas()
     {
-        $christmas = [];
+        $christmas = "";
 
-        switch ( date("w", strtotime("{$this->yr}-12-25 12:00:00")) ) {
+        switch ( date("w", strtotime("{$this->year}-12-25 12:00:00")) ) {
             case 5:
-                $christmas[] = "{$this->yr}-12-25";
-                $christmas[] = "{$this->yr}-12-28";
+                $christmas = "{$this->year}-12-25";
                 break;
             case 6:
-                $christmas[] = "{$this->yr}-12-27";
-                $christmas[] = "{$this->yr}-12-28";
+                $christmas = "{$this->year}-12-27";
                 break;
             case 0:
-                $christmas[] = "{$this->yr}-12-26";
-                $christmas[] = "{$this->yr}-12-27";
+                $christmas = "{$this->year}-12-26";
                 break;
             default:
-                $christmas[] = "{$this->yr}-12-25";
-                $christmas[] = "{$this->yr}-12-26";
+                $christmas = "{$this->year}-12-25";
         }
         return $christmas;
     }
 
-    public function allBankHolidays()
+    public function boxingDay()
     {
-        $bankHols[] = $this->newYears($this->yr);
+        $boxingDay = "";
 
-        // Good friday:
-        $bankHols[] = $this->goodFriday($this->yr);
-
-        // Easter Monday:
-        $bankHols[] = $this->easterMonday($this->yr);
-
-        // May Day:
-        $bankHols[] = $this->mayDay($this->yr);
-
-        // Whitsun:
-        $bankHols[] = $this->whitSun($this->yr);        
-
-        // Summer Bank Holiday:
-        $bankHols[] = $this->summerBankHoliday($this->yr);
-
-        // Christmas:
-        $bankHols = array_merge($bankHols, $this->christmas($this->yr));
-
-        // Millenium eve
-        if ($this->yr == 1999) {
-            $bankHols[] = "1999-12-31";
+        switch ( date("w", strtotime("{$this->year}-12-25 12:00:00")) ) {
+            case 5:
+                $boxingDay = "{$this->year}-12-28";
+                break;
+            case 6:
+                $boxingDay = "{$this->year}-12-28";
+                break;
+            case 0:
+                $boxingDay = "{$this->year}-12-27";
+                break;
+            default:
+                $boxingDay = "{$this->year}-12-26";
         }
+        return $boxingDay;
+    }
 
-        return $bankHols;
+    public function all()
+    {
+        return $this->toArray();
+    }
+    
+    public function toArray()
+    {
+        return get_object_vars($this);
+    }
+
+    public function toJson()
+    {
+        return $this->jsonSerialize();
+    }
+
+    public function jsonSerialize()
+    {
+        return json_encode($this->toArray());
     }
 
     public function __get($property)
     {
         if (property_exists($this, $property)) {
             return $this->$property;
-        } else {
-            throw new InexistentPropertyException("Inexistent property: $property");
         }
+
+        throw new InexistentPropertyException("Inexistent property: $property");
     }
 
 }
--- tests/UKBankHolidayTest.php
+++ tests/UKBankHolidayTest.php
@@ -8,25 +8,56 @@ class UKBankHolidayTest extends PHPUnit_Framework_TestCase {
     {
         $bankHolidays = new UKBankHoliday("2007");
 
-        $this->assertEquals($bankHolidays->yr, "2007");
+        $this->assertEquals($bankHolidays->year, "2007");
     }
 
   public function testGetAllBankHolidays()
   {
-    $allBankHolidays = (new UKBankHoliday("2007"))->allBankHolidays();
+    $allBankHolidays = (new UKBankHoliday("2007"))->all();
     
     $expectedDates = [
-        "2007-01-01",
-        "2007-04-06",
-        "2007-04-09",
-        "2007-05-07",
-        "2007-05-28",
-        "2007-08-27",
-        "2007-12-25",
-        "2007-12-26",
+        'year'           => '2007',
+        'newYears'           => '2007-01-01',
+        'goodFriday'         => '2007-04-06',
+        'easterMonday'       => '2007-04-09',
+        'mayDay'             => '2007-05-07',
+        'whitSun'            => '2007-05-28',
+        'summerBankHoliday' => '2007-08-27',
+        'christmas'           => '2007-12-25',
+        'boxingDay'          => '2007-12-26',
+        'milleniumEve'          => null,
     ];
 
     $this->assertEquals($allBankHolidays, $expectedDates);
+  }
+
+  public function testBankHolidaysToArray()
+  {
+    $bankHolidaysArray = (new UKBankHoliday("2007"))->toArray();
+
+    $expectedDates = [
+        'year'           => '2007',
+        'newYears'           => '2007-01-01',
+        'goodFriday'         => '2007-04-06',
+        'easterMonday'       => '2007-04-09',
+        'mayDay'             => '2007-05-07',
+        'whitSun'            => '2007-05-28',
+        'summerBankHoliday' => '2007-08-27',
+        'christmas'           => '2007-12-25',
+        'boxingDay'          => '2007-12-26',
+        'milleniumEve'          => null,
+    ];
+
+    $this->assertEquals($bankHolidaysArray, $expectedDates);
+  }
+
+  public function testBankHolidaysToJson()
+  {
+    $bankHolidaysJson = (new UKBankHoliday("2007"))->toJson();
+
+    $expectedDatesJson = '{"year":"2007","newYears":"2007-01-01","goodFriday":"2007-04-06","easterMonday":"2007-04-09","mayDay":"2007-05-07","whitSun":"2007-05-28","summerBankHoliday":"2007-08-27","christmas":"2007-12-25","boxingDay":"2007-12-26","milleniumEve":null}';
+
+    $this->assertEquals($bankHolidaysJson, $expectedDatesJson);
   }
  
 }
